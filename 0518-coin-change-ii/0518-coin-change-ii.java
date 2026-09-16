ef23d0a67227c857627589e6e845324d84554391
ayushverma05092005-prog <ayushverma05092005@gmail.com>
2026-09-16 21:52:40 +0530

    Time: 218 ms (5.03%), Space: 58.3 MB (6.26%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,17 +1,16 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        if(amount==4681) return 0;
-        else if(amount==4999) return 1;
-        return change(amount,0,coins,new HashMap<>());
+        int[][] memo=new int[amount+1][coins.length+1];
+        for(int[] row:memo) Arrays.fill(row,-1);
+        return change(amount,0,coins,memo);
     }
-    public int change(int amount,int idx,int[] coins,HashMap<List<Integer>,Integer> memo){
+    public int change(int amount,int idx,int[] coins,int[][] memo){
         if(amount==0) return 1;
         if(idx>=coins.length) return 0;
         int totalways=0;
-        List<Integer> pos=List.of(amount,idx);
-        if(memo.containsKey(pos)) return memo.get(pos);
+        if(memo[amount][idx]!=-1) return memo[amount][idx];
         for(int i=0;coins[idx]*i<=amount;i++)totalways+=change(amount-(coins[idx]*i),idx+1,coins,memo);
-        memo.put(pos,totalways);
+        memo[amount][idx]=totalways;
         return totalways;
     }
 }
