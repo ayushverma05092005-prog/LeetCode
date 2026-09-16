ac54e34402bb4442f2d51dc8f314c465fe3d3244
ayushverma05092005-prog <ayushverma05092005@gmail.com>
2026-09-16 21:55:51 +0530

    Time: 0 ms (100%), Space: 42.2 MB (47.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return uniquePaths(1,1,m,n,new HashMap<>());
+        int[][] memo=new int[m+1][n+1];
+        for(int[] row:memo) Arrays.fill(row,-1);
+        return uniquePaths(1,1,m,n,memo);
     }
-    public int uniquePaths(int i,int j,int m,int n, HashMap<List<Integer>,Integer>memo){
+    public int uniquePaths(int i,int j,int m,int n, int[][] memo){
         if(i==m && j==n) return 1;
         if(i>m || j>n) return 0;
-        List<Integer> pos=List.of(i,j);
-        if(memo.containsKey(pos)) return memo.get(pos);
+        if(memo[i-1][j-1]!=-1) return memo[i-1][j-1];
         int total=uniquePaths(i+1,j,m,n,memo)+uniquePaths(i,j+1,m,n,memo);
-        memo.put(pos,total);
+        memo[i-1][j-1]=total;
         return total;
     }
 }
