7c2f361adbcb937f6d62cdf536fec32b031db2e5
ayushverma05092005-prog <ayushverma05092005@gmail.com>
2026-09-16 21:58:43 +0530

    Time: 1 ms (99.81%), Space: 50.2 MB (72.42%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,16 +1,17 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        return minPathSum(0,0,grid,new HashMap<>());
+        int [][] memo=new int[grid.length+1][grid[0].length+1];
+        for(int[] row:memo) Arrays.fill(row,-1);
+        return minPathSum(0,0,grid,memo);
     }
-    public int minPathSum(int i,int j,int[][] grid,HashMap<List<Integer>,Integer> memo){
+    public int minPathSum(int i,int j,int[][] grid,int[][] memo){
         if(i==grid.length-1 && j==grid[0].length-1) return grid[i][j];
         if(i>=grid.length || j>= grid[0].length) return Integer.MAX_VALUE;
-        List<Integer> pos=List.of(i,j);
-        if(memo.containsKey(pos)) return memo.get(pos);
+        if(memo[i][j]!=-1) return memo[i][j];
         int mini=Integer.MAX_VALUE;
         mini=Math.min(minPathSum(i+1,j,grid,memo),minPathSum(i,j+1,grid,memo));
         if(mini!=Integer.MAX_VALUE) mini=mini+grid[i][j];
-        memo.put(pos,mini);
+        memo[i][j]=mini;
         return mini;
     }
 }
