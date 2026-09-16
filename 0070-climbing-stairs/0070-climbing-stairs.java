e20eb8ceef166d8ad6b1515b291e2a08e26aa874
ayushverma05092005-prog <ayushverma05092005@gmail.com>
2026-09-16 21:37:54 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        return climbStairs(n,new HashMap<>());
+        int[] memo=new int[n+1];
+        Arrays.fill(memo,-1);
+        return climbStairs(n,memo);
     }
-    public int climbStairs(int n,HashMap<Integer,Integer> memo){
+    public int climbStairs(int n,int[] memo){
         if(n==0) return 1;
         if(n<0) return 0;
-        if(memo.containsKey(n)) return memo.get(n);
+        if(memo[n]!=-1) return memo[n];
         int result= climbStairs(n-1,memo)+climbStairs(n-2,memo);
-        memo.put(n,result);
+        memo[n]=result;
         return result;
     }
 }
