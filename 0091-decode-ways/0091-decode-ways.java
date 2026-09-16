999719894abaeda7021a97c5b1cda793c5a8474d
ayushverma05092005-prog <ayushverma05092005@gmail.com>
2026-09-16 21:44:36 +0530

    Time: 1 ms (81.61%), Space: 43 MB (64.06%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -2,17 +2,18 @@ class Solution {
     public int numDecodings(String s) {
         if (s.charAt(0) == '0') return 0;
         if(s.length()==1 ) return 1;
-        HashMap<Integer,Integer> memo=new HashMap<>();
+        int[] memo=new int[s.length()+1];
+        Arrays.fill(memo,-1);
         return numDecodings(1,Integer.parseInt(s.substring(0,1)),s,memo)+numDecodings(2,Integer.parseInt(s.substring(0,2)),s,memo);
     }
-    public int numDecodings(int i, int n,String s, HashMap<Integer, Integer> memo) {
+    public int numDecodings(int i, int n,String s, int[] memo) {
         if(n<=0 || n>26)return 0;
         if(i==s.length()) return 1;
         if(s.charAt(i)=='0') return 0;
         if(i==s.length()-1) return 1;
-        if(memo.containsKey(i)) return memo.get(i);
+        if(memo[i]!=-1) return memo[i];
         int result=numDecodings(i+1,Integer.parseInt(s.substring(i,i+1)),s,memo)+numDecodings(i+2,Integer.parseInt(s.substring(i,i+2)),s,memo);
-        memo.put(i,result);
+        memo[i]=result;
         return result;
     }
     
