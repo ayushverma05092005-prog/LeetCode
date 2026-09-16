425776f99cc4f5ffc3acc3e2f2d078453067595a
ayushverma05092005-prog <ayushverma05092005@gmail.com>
2026-09-16 21:40:15 +0530

    Time: 1 ms (39.44%), Space: 44.8 MB (75.65%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,14 +1,15 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        HashMap<Integer,Integer> memo=new HashMap<>();
+        int[] memo=new int[cost.length+1];
+        Arrays.fill(memo,-1);
         return Math.min(minCostClimbingStairs(0,cost,memo),minCostClimbingStairs(1,cost,memo));
     }
-    public int minCostClimbingStairs(int i,int[] cost,HashMap<Integer,Integer> memo){
+    public int minCostClimbingStairs(int i,int[] cost,int[] memo){
         if(i==cost.length-1) return cost[i];
         if(i>=cost.length) return 0;
-        if(memo.containsKey(i)) return memo.get(i);
+        if(memo[i]!=-1) return memo[i];
         int result= cost[i]+Math.min(minCostClimbingStairs(i+1,cost,memo),minCostClimbingStairs(i+2,cost,memo));
-        memo.put(i,result);
+        memo[i]=result;
         return result;
     }
 }
