3ea7609b4c93ea923972b06a33123cfe5ddc8ee6
ayushverma05092005-prog <ayushverma05092005@gmail.com>
2026-09-16 21:48:40 +0530

    Time: 25 ms (32.53%), Space: 46.6 MB (44.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,18 +1,20 @@
 class Solution {
     public int coinChange(int[] coins,int amount){
-        int res=coinChange(coins,amount,new HashMap<>());
+        int[] memo=new int[amount+1];
+        Arrays.fill(memo,-2);
+        int res=coinChange(coins,amount,memo);
         return res==Integer.MAX_VALUE?-1:res;
     }
-    public int coinChange(int[] coins, int amount,HashMap<Integer,Integer> memo) {
+    public int coinChange(int[] coins, int amount,int[] memo) {
         if(amount==0) return 0;
-        if(memo.containsKey(amount)) return memo.get(amount);
+        if(memo[amount]!=-2) return memo[amount];
         int mini=Integer.MAX_VALUE;
         for(int num:coins){
             int remain=amount-num;
             if(remain>=0) mini=Math.min(coinChange(coins,remain,memo),mini);
         }
-        if(mini==Integer.MAX_VALUE) {memo.put(amount,mini);return mini;}
-        memo.put(amount,mini+1);
+        if(mini==Integer.MAX_VALUE) {memo[amount]=mini;return mini;}
+        memo[amount]=mini+1;
         return mini+1;
     }
 }
